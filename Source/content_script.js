62ec6259a0fabadf2746acafe27884361c85b331
Jonathan Reyes <jonathan.js.reyes@gmail.com>
2015-12-04 21:54:07 -0500

    Was working, but attempted to refine question insertion
    Working code still there, but commented out

--- Source/content_script.js
+++ Source/content_script.js
@@ -36,19 +36,49 @@ function handleText(textNode)
 	v = v.replace(/\belite\b/g, "Joe Flacco");
 	v = v.replace(/\bELITE\b/g, "JOE FLACCO");
 	
-	/* Insert "Is Joe Flacco Elite?" into any string of questions
-	var split_v = v.replace(/([.?!])\s*(?=[A-Z])/, "$1|").split("|");
-	for (var i = 0; i < split_v.length - 1; i++) {
-		if (split_v.substr(split_v[i].length - 1) === '?' && split_v.substr(split_v[i + 1].length - 1) === '?') {
-			split_v.splice(i + 1, 0, "Is Joe Flacco Elite?");
+	/*Insert "Is Joe Flacco Elite?" into any string of questions*/
+	//Split the v into individual sentences
+	var sentences = v.replace(/([.?!])\s*(?=[a-zA-Z])/, "$1|").split("|");
+
+	var i = 0;
+	while (i < sentences.length - 1) {
+		console.log(sentences[i]);
+
+		//Insert "Is Joe Flacco Elite?" between the first pair of questions
+		// if (sentences[i].substr(sentences[i].length - 1) === '?' && sentences[i+1].substr(sentences[i+1].length - 1) === '?') {
+		// 	sentences.splice(++i, 0, " Is Joe Flacco Elite?");
+		// 	break;
+		// }
+
+		//find the limit indices of a run of questions
+		var questionIdxMin = -1, questionIdxMax = -1;
+		if (sentences[i].substr(sentences[i].length - 1) === '?' 
+			  && sentences[i+1].substr(sentences[i+1].length - 1) === '?') {
+
+			questionIdxMin = ++i; //save the index of the second question (first index where we might insert our question)
+
+			//this while loop ends when we've svaed the index of the last question
+			while (sentences[i].substr(sentences[i].length - 1) === '?' 
+					   && sentences[i+1].substr(sentences[i+1].length - 1) === '?'
+					   && i < sentence.length - 1) {
+				questionIdxMax = i++;
+			}
+
+			//generate a random index between the limits of the run of questions
+			var insertionIdx = Math.floor(Math.random() * (questionIdxMax - questionIdxMin + 1)) + questionIdxMin;
+		
+			//insert the question at the random index
+			sentences.splice(insertionIdx, 0, "Is Joe Flacco Elite?");
+
+			//only want to put the question in once per textNode
+			break;
 		}
 		
+
+		i++;
 	}
 	
-	textNode.nodeValue = split_v.join("");
-	*/
-	
-	textNode.nodeValue = v;
+	textNode.nodeValue = sentences.join("");
 }
 
 
